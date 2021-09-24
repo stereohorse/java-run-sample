4a1600164ae2b6cedb882844f2cef1beb7e76e33
Viet Nguyen <me@tuanviet.ru>
2021-09-24 16:51:26 +0300

    feat: demonstrate pair ping pong

--- src/main/java/ru/tuanviet/javabox/App.java
+++ src/main/java/ru/tuanviet/javabox/App.java
@@ -1,7 +1,29 @@
 package ru.tuanviet.javabox;
 
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
+        // search max value from list
+    }
 
+    public static Integer max(List<Integer> numbers) {
+        if (numbers == null) {
+            throw new IllegalArgumentException("Input list is null");
+        }
+
+        if (numbers.isEmpty()) {
+            throw new IllegalArgumentException("Imput list is empty");
+        }
+
+        int max = Integer.MIN_VALUE;
+
+        for (Integer num : numbers) {
+            if (max < num) {
+                max = num;
+            }
+        }
+
+        return max;
     }
 }
--- src/test/java/ru/tuanviet/javabox/AppTest.java
+++ src/test/java/ru/tuanviet/javabox/AppTest.java
@@ -3,12 +3,51 @@ package ru.tuanviet.javabox;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.junit.Test;
 
 public class AppTest {
 
     @Test
-    public void shouldAnswerWithTrue() {
-        assertThat(true).isTrue();
+    public void shouldProcessSingleValueList() {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(10);
+        Integer max = App.max(numbers);
+
+        assertThat(max).isEqualTo(10);
+    }
+
+    @Test
+    public void shouldProcessMultipleValueList() {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(10);
+        numbers.add(16);
+        Integer max = App.max(numbers);
+
+        assertThat(max).isEqualTo(16);
+    }
+
+    @Test
+    public void shouldProcessUnsortedList() {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(27);
+        numbers.add(10);
+        numbers.add(16);
+        Integer max = App.max(numbers);
+
+        assertThat(max).isEqualTo(27);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowOnNullInputList() {
+        App.max(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowOnEmptyInputList(){
+        App.max(Collections.emptyList());
     }
 }
