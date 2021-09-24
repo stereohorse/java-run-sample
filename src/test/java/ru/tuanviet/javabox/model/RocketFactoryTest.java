181275e55409bd0d973a255e5bd056169d7e9c74
Viet Nguyen <me@tuanviet.ru>
2021-09-24 13:39:06 +0300

    test: add example

--- src/test/java/ru/tuanviet/javabox/model/RocketFactoryTest.java
+++ src/test/java/ru/tuanviet/javabox/model/RocketFactoryTest.java
@@ -2,10 +2,17 @@ package ru.tuanviet.javabox.model;
 
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class RocketFactoryTest {
 
     @Test
     public void shouldMakeRockets() {
         // TODO: write the test here
+    }
+
+    @Test
+    public void exampleTest() {
+        assertThat(true).isTrue();
     }
 }
