a851b69fcc0a34024fd6518d387af27575931096
Viet Nguyen <me@tuanviet.ru>
2021-10-01 18:10:08 +0300

    feat: setup task 2 for generics

--- src/main/java/ru/tuanviet/javabox/App.java
+++ src/main/java/ru/tuanviet/javabox/App.java
@@ -4,18 +4,8 @@ public class App {
     public static void main(String[] args) {
 
     }
+}
 
+class Shawarma {
     // TODO: implement method here
-}
-
-class Duck {
-    String say() {
-        return "Release the Kraken!";
-    }
-}
-
-class Penguin {
-    String say() {
-        return "All your base are belong to us";
-    }
 }
