c61895f9bef9c38d3a657eb3f9ee7cda5749148a
Viet Nguyen <me@tuanviet.ru>
2021-10-01 17:54:06 +0300

    feat: setup task 1 for generics

--- src/main/java/ru/tuanviet/javabox/App.java
+++ src/main/java/ru/tuanviet/javabox/App.java
@@ -4,4 +4,18 @@ public class App {
     public static void main(String[] args) {
 
     }
+
+    // TODO: implement method here
+}
+
+class Duck {
+    void say() {
+        System.out.println("Release the Kraken!");
+    }
+}
+
+class Penguin {
+    void say() {
+        System.out.println("All your base are belong to us");
+    }
 }
