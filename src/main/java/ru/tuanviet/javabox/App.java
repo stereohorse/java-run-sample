5f23f934256327c29c58025e10a00d3c8a6dbac4
Viet Nguyen <me@tuanviet.ru>
2021-10-05 19:03:57 +0300

    feat: setup task 4

--- src/main/java/ru/tuanviet/javabox/App.java
+++ src/main/java/ru/tuanviet/javabox/App.java
@@ -6,6 +6,22 @@ public class App {
     }
 }
 
-class Shawarma {
-    // TODO: implement method here
+class Shawarma<T> {
+    private T value;
+
+    public Shawarma(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    // TODO: input 2 args: source Shawarma, dest Shawarma
+    // TODO: dest should contain source value
+    // TODO: name: copy
 }
